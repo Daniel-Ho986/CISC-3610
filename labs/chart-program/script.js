e1d8e40c6fb00731a7d732a5850869f96c6face4
Daniel <dhonaruto@gmail.com>
2022-02-15 16:17:23 -0500

    complete chart program lab

--- labs/chart-program/script.js
+++ labs/chart-program/script.js
@@ -11,20 +11,34 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 
 function draw() {
-    let position = 0;  
+    let barPosition = 0;  
+    let labelPosition = 20;
+
+    ctx.font= "bold 20px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Daniel Ho", 140, 20);
 
     // Loop through the array
     // Call drawBar() function to draw each one
     for (let obj of fruit) {
-        
-        drawBar(ctx, obj.name, obj.quantity*8, obj.color, position);
-        position += 40;
+        drawBar(ctx, obj.name, obj.quantity, obj.color, barPosition, labelPosition);
+        barPosition += 60;
+        labelPosition += 60;
     } 
 }
 
-//set new origin
-// or flip sx sy
-function drawBar(ctx, name, quantity, color, position) {
+
+function drawBar(ctx, name, quantity, color, barPosition, labelPosition) {
+    // Draw Bar
     ctx.fillStyle = color;
-    ctx.fillRect(position, canvas.height - quantity, 40, quantity)
+    ctx.fillRect(barPosition, canvas.height - quantity * 15, 60, quantity * 15);
+
+    // Write Label
+    ctx.font= "bold 12px Arial";
+    ctx.fillStyle = "black";
+    var line = 7;
+    var height = 38.5;
+    ctx.fillText(quantity, labelPosition, height * line);
+    line += 0.5;
+    ctx.fillText(name, labelPosition - 10, height * line);
 }
