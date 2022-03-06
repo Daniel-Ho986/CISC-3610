9cdaf6414447360ddd4773484b1f77e7ffd95f69
Daniel <dhonaruto@gmail.com>
2022-03-06 15:27:48 -0500

    set files for numbers-animation

---
+++ labs/number-animation/numbers.js
@@ -0,0 +1,219 @@
+numbers = {
+    src: "spritesheet.png",
+    img: undefined,
+    frame: 0,
+    offset: 0,
+	"frames": {
+		"tile000.png": {
+			"frame": {
+				"x": 1,
+				"y": 1,
+				"w": 118,
+				"h": 148
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 148
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 148
+			}
+		},
+		"tile001.png": {
+			"frame": {
+				"x": 121,
+				"y": 1,
+				"w": 118,
+				"h": 148
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 148
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 148
+			}
+		},
+		"tile002.png": {
+			"frame": {
+				"x": 241,
+				"y": 1,
+				"w": 118,
+				"h": 148
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 148
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 148
+			}
+		},
+		"tile003.png": {
+			"frame": {
+				"x": 1,
+				"y": 151,
+				"w": 118,
+				"h": 148
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 148
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 148
+			}
+		},
+		"tile004.png": {
+			"frame": {
+				"x": 121,
+				"y": 151,
+				"w": 118,
+				"h": 148
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 148
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 148
+			}
+		},
+		"tile005.png": {
+			"frame": {
+				"x": 241,
+				"y": 151,
+				"w": 118,
+				"h": 147
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 147
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 147
+			}
+		},
+		"tile006.png": {
+			"frame": {
+				"x": 361,
+				"y": 1,
+				"w": 118,
+				"h": 147
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 147
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 147
+			}
+		},
+		"tile007.png": {
+			"frame": {
+				"x": 361,
+				"y": 150,
+				"w": 118,
+				"h": 147
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 147
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 147
+			}
+		},
+		"tile008.png": {
+			"frame": {
+				"x": 361,
+				"y": 299,
+				"w": 118,
+				"h": 147
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 147
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 147
+			}
+		},
+		"tile009.png": {
+			"frame": {
+				"x": 481,
+				"y": 1,
+				"w": 118,
+				"h": 147
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 118,
+				"h": 147
+			},
+			"sourceSize": {
+				"w": 118,
+				"h": 147
+			}
+		}
+	},
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "spritesheet.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 600,
+			"h": 447
+		},
+		"scale": "1"
+	}
+}
