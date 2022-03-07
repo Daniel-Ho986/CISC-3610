fec6a6a9ac79b2651eb2a07c4d273e298c7762ae
Daniel <dhonaruto@gmail.com>
2022-03-06 19:06:07 -0500

    Completed assignment

--- labs/number-animation/animation.js
+++ labs/number-animation/animation.js
@@ -6,6 +6,8 @@ var Scene = {
 	sprite: undefined
 };
 
+var counter = 0;
+
 Scene.start = function () {
 	// Get the canvas and it's context.
     Scene.canvas = document.getElementById("myCanvas");
@@ -21,7 +23,19 @@ Scene.start = function () {
 	// Wait till the numbers image is loaded before starting the animation.
 	Scene.sprite.img.onload = function() {		
 		Scene.sprite.offset=-Scene.sprite.frames[Scene.sprite.frame].frame.w;
-    	Scene.mainLoop();
+    	// Scene.mainLoop();
+
+		Scene.canvasContext.drawImage(
+			Scene.sprite.img,
+			Scene.sprite.frames[0].frame.x,
+			Scene.sprite.frames[0].frame.y,
+			Scene.sprite.frames[0].frame.w,
+			Scene.sprite.frames[0].frame.h,
+			250,
+			100,
+			Scene.sprite.frames[0].frame.w,
+			Scene.sprite.frames[0].frame.h
+		);
 	}
 };
 
@@ -29,7 +43,7 @@ Scene.start = function () {
 document.addEventListener( 'DOMContentLoaded', Scene.start);
 
 Scene.clearCanvas = function () {
-    Scene.canvasContext.fillStyle = "white";
+    Scene.canvasContext.fillStyle = "black";
     Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
 };
 
@@ -37,14 +51,18 @@ Scene.mainLoop = function() {
     Scene.clearCanvas();
     Scene.update();
     Scene.draw();
-	
-	// Animate at 24 frames a second.
-    window.setTimeout(Scene.mainLoop, 1000 / 6);
+
+	counter++;
+
+	// Animate at 1 frames a second.
+	if (counter < 11) {
+    	window.setTimeout(Scene.mainLoop, 500);
+	}
 };
 
 Scene.update = function () {
 	// Set the canvas width to be that of the display Window. Which helps if you resize the window.
-  	Scene.canvas.width = window.innerWidth;
+  	// Scene.canvas.width = window.innerWidth;
 	
 	// Set the location of the next frame. 
   	Scene.sprite.offset+=24;
@@ -53,12 +71,48 @@ Scene.update = function () {
 };
 
 Scene.draw = function () {
-	Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[Scene.sprite.frame].frame.x,Scene.sprite.frames[Scene.sprite.frame].frame.y,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h);
+	if (Scene.sprite.frame < Scene.sprite.frames.length) {
+		Scene.canvasContext.drawImage(
+			Scene.sprite.img,
+			Scene.sprite.frames[Scene.sprite.frame].frame.x,
+			Scene.sprite.frames[Scene.sprite.frame].frame.y,
+			Scene.sprite.frames[Scene.sprite.frame].frame.w,
+			Scene.sprite.frames[Scene.sprite.frame].frame.h,
+			250,
+			100,
+			Scene.sprite.frames[Scene.sprite.frame].frame.w,
+			Scene.sprite.frames[Scene.sprite.frame].frame.h
+		);
 	
-	// Advance to the next frame.
-	Scene.sprite.frame++;
+		// Advance to the next frame.
+		Scene.sprite.frame++;
+	}
 
-	// At the end of the sprite sheet, start at the first frame.
-	if(Scene.sprite.frame==Scene.sprite.frames.length)
-		Scene.sprite.frame=0;
+	// At the end of the sprite sheet, start at the 10 frame.
+	else {
+		Scene.sprite.frame = 1;
+		Scene.canvasContext.drawImage(
+			Scene.sprite.img,
+			Scene.sprite.frames[Scene.sprite.frame].frame.x,
+			Scene.sprite.frames[Scene.sprite.frame].frame.y,
+			Scene.sprite.frames[Scene.sprite.frame].frame.w,
+			Scene.sprite.frames[Scene.sprite.frame].frame.h,
+			180,
+			100,
+			Scene.sprite.frames[Scene.sprite.frame].frame.w,
+			Scene.sprite.frames[Scene.sprite.frame].frame.h
+		);
+		Scene.sprite.frame = 0;
+		Scene.canvasContext.drawImage(
+			Scene.sprite.img,
+			Scene.sprite.frames[Scene.sprite.frame].frame.x,
+			Scene.sprite.frames[Scene.sprite.frame].frame.y,
+			Scene.sprite.frames[Scene.sprite.frame].frame.w,
+			Scene.sprite.frames[Scene.sprite.frame].frame.h,
+			295,
+			100,
+			Scene.sprite.frames[Scene.sprite.frame].frame.w,
+			Scene.sprite.frames[Scene.sprite.frame].frame.h
+		);
+	}
 };
