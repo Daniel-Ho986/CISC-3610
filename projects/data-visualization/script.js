88386998075b4e83eb488390a35141d54404ebc8
Daniel <dhonaruto@gmail.com>
2022-03-16 16:45:45 -0400

    completed first chartjs chart

--- projects/data-visualization/script.js
+++ projects/data-visualization/script.js
@@ -1,19 +1,68 @@
-new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
+const DONUT = document.getElementById("donutChart");
+const HBAR = document.getElementById("hBarChart");
+
+let donutChart = new Chart(DONUT, {
+  type: 'doughnut',
+  data: {
+    labels: [
+      'Brooklyn Studio Secondary School',
+      'Brooklyn Technical High School',
+      'Edward R. Murrow High School',
+      'Forest Hills High School',
+      'Fort Hamilton High School',
+      'Franklin Delano Roosevelt High School',
+      'James Madison High School',
+      'John Dewey High School',
+      'Life Academy High School For Film and Music',
+      'Midwood High School',
+      'Stuyvesant High School'
+    ],
+    datasets: [{
+      label: '2012 Num of SAT Test Takers',
+      data: [119, 1277, 727, 762, 694, 385, 518, 448, 40, 824, 832],
+      backgroundColor: [
+        'rgb(255, 102, 102)',
+        'rgb(255, 178, 102)',
+        'rgb(255, 255, 102)',
+        'rgb(178, 255, 102)',
+        'rgb(102, 255, 102)',
+        'rgb(102, 255, 178)',
+        'rgb(102, 255, 255)',
+        'rgb(102, 178, 255)',
+        'rgb(102, 102, 255)',
+        'rgb(178, 102, 255)',
+        'rgb(255, 102, 255)'
+      ],
+      hoverOffset: 15,
+    }]
+  },
+  options: {
+    plugins: {
       title: {
         display: true,
-        text: 'Predicted world population (millions) in 2050'
+        text: '2012 Num of SAT Test Takers',
+        font: {
+            size: 15,
+        }
+      },
+      legend: {
+        position: 'bottom',
+        labels: {
+          textAlign: 'center',
+          
+        }
       }
     }
-});
+  }
+
+})
+
+let hBarChart = new Chart(HBAR, {
+  type: 'bar',
+  data: {
+    labels: ['']
+  },
+  options: {
+    indexAxis: 'y',
+  }
+})
