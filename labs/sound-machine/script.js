17135cb0c266f8913128048d9097ed7d2594fd83
Daniel <dhonaruto@gmail.com>
2022-04-29 11:11:59 -0400

    completed sound machine

---
+++ labs/sound-machine/script.js
@@ -0,0 +1,29 @@
+const chimes = document.getElementById("chimes");
+const playChimes = () => {
+    console.log("play chimes");
+    chimes.play();
+};
+
+const drum = document.getElementById("drum");
+const playDrum = () => {
+    console.log("play drum");
+    drum.play();
+};
+
+const flute = document.getElementById("flute");
+const playFlute = () => {
+    console.log("play flute");
+    flute.play();
+};
+
+const guitar = document.getElementById("guitar");
+const playGuitar = () => {
+    console.log("play guitar");
+    guitar.play();
+};
+
+const violin = document.getElementById("violin");
+const playViolin = () => {
+    console.log("play violin");
+    violin.play();
+};
